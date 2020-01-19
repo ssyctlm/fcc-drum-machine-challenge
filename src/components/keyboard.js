c0560038e5d7ee374015741b30aa5e3089aac706
hellow <myqy480@gmail.com>
2020-01-19 18:02:09 +0800

    tring

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class KeyBoard extends Component{
   constructor(props){
-    super(props)
+    super(props);
 
   }
 
@@ -13,7 +13,8 @@ export default class KeyBoard extends Component{
         <div 
         className={"keyboard " + this.props.newstyle }
         onClick ={this.props.onClick}
-        >
+        ><audio id = {this.props.id} src={this.props.sound} type = "audio/mp3" onPlay = (this.props.onClick)>
+            </audio>
           {this.props.letter}
         </div>
       </React.Fragment>
--- src/components/toggle.js
+++ src/components/toggle.js
@@ -9,12 +9,12 @@ export default class Toggle extends Component {
     handleClick = () => this.setState({ flag: !this.state.flag })
 
     render() {
-        const classChange = this.state.flag ? ["toggleBtn on", "toggleBtn off"] : ["toggleBtn off", "toggleBtn on"]
+        const classChange = this.state.flag ? [["toggleBtn on","ON"], ["toggleBtn off",""]] : [["toggleBtn off",""], ["toggleBtn on","OFF"]]
         return (
 
             <div className="toggleBox" onClick={this.handleClick}>
-                <div className={classChange[0]}></div>
-                <div className={classChange[1]}></div>
+        <div className={classChange[0][0]}>{classChange[0][1]}</div>
+        <div className={classChange[1][0]}>{classChange[1][1]}</div>
             </div >
 
         )
